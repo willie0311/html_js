446d47e897dfafa554ea400a1db5dc216180fdbb
willie <willie@gmail.com>
2022-05-23 15:11:41 +0800

    index增加

--- script.js
+++ script.js
@@ -1,20 +1,14 @@
-document.write("test");
-alert('測試');
-console.log('123');
-confirm('對錯');
-prompt('你幾歲','25歲');
-//
-/* */
-/**
- * 
- * 
- * 
- */
-//ver
+let myName =prompt("請輸入你的名字:");
+let myAge =prompt("請輸入你的年齡:");
+let myHeight =prompt("請輸入你的身高:");
+let myWeight =prompt("請輸入你的體重:");
 
+document.write('大家好，我是'+ myName +',<br>');
+document.write('身高'+ myHeight +'公分,體重'+ myWeight +'公斤,<br>');
+document.write('今年'+ myAge +'歲。');
 
-//let
+let x = "1981";
+let y = 701023
 
-
-//const
-
+console.log(Number(x)+y);
+console.log(x+y.toString());
